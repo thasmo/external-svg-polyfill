96d93e8c42caa1666028bfe20af92691c0c491fa
Thomas Deinhamer <thasmo@thasmo.com>
2019-05-11 13:39:52 +0200

    Provide a minified browser version

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,34 @@
 import typescript from 'rollup-plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
 
-export default {
-	input: 'src/index.ts',
-	output: {
-		file: 'browser/bundle.js',
-		format: 'umd',
-		name: 'ExternalSvgPolyfill',
-		sourcemap: true,
+export default [
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'browser/bundle.js',
+			format: 'umd',
+			name: 'ExternalSvgPolyfill',
+			sourcemap: true,
+		},
+		plugins: [
+			typescript({
+				target: 'es5',
+			}),
+		],
 	},
-	plugins: [
-		typescript({
-			target: 'es5',
-		}),
-	],
-}
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'browser/bundle.min.js',
+			format: 'umd',
+			name: 'ExternalSvgPolyfill',
+			sourcemap: true,
+		},
+		plugins: [
+			typescript({
+				target: 'es5',
+			}),
+			terser(),
+		],
+	},
+];
